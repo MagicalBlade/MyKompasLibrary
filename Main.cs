8440d1234bba1b5b99f10d28b756799e78bb64a4
Александр Воскобойник <magical.blade@gmail.com>
2023-03-02 19:41:29 +0300

    Добавьте файлы проекта.

---
+++ Main.cs
@@ -0,0 +1,151 @@
+﻿using Kompas6Constants;
+using Kompas6API5;
+using KompasAPI7;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Reflection;
+using Microsoft.Win32;
+using System.Windows.Forms;
+
+namespace MyKompasLibrary
+{
+    public class Main
+    {
+        KompasObject kompas;
+        IApplication application;
+        IKompasDocument activeDocument;
+        // Имя библиотеки
+        [return: MarshalAs(UnmanagedType.BStr)]
+        public string GetLibraryName()
+        {
+            return "Моя библиотека";
+        }
+
+        [return: MarshalAs(UnmanagedType.BStr)]
+        #region Формируем меню команд
+        public string ExternalMenuItem(short number, ref short itemType, ref short command)
+        {
+            string result = string.Empty;
+            itemType = 1; // "MENUITEM"
+            switch (number)
+            {
+                case 1:
+                    result = "Закрыть не сохраняясь";
+                    command = 1;
+                    break;
+                case 2:
+                    result = "Тест";
+                    command = 1;
+                    break;
+                case 3:
+                    command = -1;
+                    itemType = 8; // "ENDMENU"
+                    break;
+            }
+            return result;
+        }
+
+        #endregion
+        /// <summary>
+        /// Закрыть документ не сохраняясь
+        /// </summary>
+        private void CloseNoSave()
+        {
+            activeDocument.Close(DocumentCloseOptions.kdDoNotSaveChanges);
+        }
+        private void Test()
+        {
+            ksDocument2D kompasDocument = kompas.ActiveDocument2D();
+            ksLibStyle ksLibStyle = kompas.GetParamStruct(77);
+            MessageBox.Show($"{kompasDocument.ksIsStyleInDocument(1, ksLibStyle, 1)}");;
+        }
+
+        // Головная функция библиотеки
+        public void ExternalRunCommand([In] short command, [In] short mode, [In, MarshalAs(UnmanagedType.IDispatch)] object kompas_)
+        {
+            kompas = (KompasObject)kompas_;
+            application = (IApplication)kompas.ksGetApplication7();
+            activeDocument = application.ActiveDocument;
+            //Вызываем команды
+            switch (command)
+            {
+                case 1: CloseNoSave(); break; 
+                case 2: Test(); break; 
+            }
+        }
+
+
+
+        public object ExternalGetResourceModule()
+        {
+            return Assembly.GetExecutingAssembly().Location;
+        }
+
+        public int ExternalGetToolBarId(short barType, short index)
+        {
+            int result = 0;
+
+            if (barType == 0)
+            {
+                result = -1;
+            }
+            else
+            {
+                switch (index)
+                {
+                    case 1:
+                        result = 3001;
+                        break;
+                    case 2:
+                        result = -1;
+                        break;
+                }
+            }
+
+            return result;
+        }
+
+
+        #region COM Registration
+        // Эта функция выполняется при регистрации класса для COM
+        // Она добавляет в ветку реестра компонента раздел Kompas_Library,
+        // который сигнализирует о том, что класс является приложением Компас,
+        // а также заменяет имя InprocServer32 на полное, с указанием пути.
+        // Все это делается для того, чтобы иметь возможность подключить
+        // библиотеку на вкладке ActiveX.
+        [ComRegisterFunction]
+        public static void RegisterKompasLib(Type t)
+        {
+            try
+            {
+                RegistryKey regKey = Registry.LocalMachine;
+                string keyName = @"SOFTWARE\Classes\CLSID\{" + t.GUID.ToString() + "}";
+                regKey = regKey.OpenSubKey(keyName, true);
+                regKey.CreateSubKey("Kompas_Library");
+                regKey = regKey.OpenSubKey("InprocServer32", true);
+                regKey.SetValue(null, System.Environment.GetFolderPath(Environment.SpecialFolder.System) + @"\mscoree.dll");
+                regKey.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(string.Format("При регистрации класса для COM-Interop произошла ошибка:\n{0}", ex));
+            }
+        }
+
+        // Эта функция удаляет раздел Kompas_Library из реестра
+        [ComUnregisterFunction]
+        public static void UnregisterKompasLib(Type t)
+        {
+            RegistryKey regKey = Registry.LocalMachine;
+            string keyName = @"SOFTWARE\Classes\CLSID\{" + t.GUID.ToString() + "}";
+            RegistryKey subKey = regKey.OpenSubKey(keyName, true);
+            subKey.DeleteSubKey("Kompas_Library");
+            subKey.Close();
+        }
+        #endregion
+    }
+}
